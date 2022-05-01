564c4bf6b86ad9bfa972658626aa5d45daae7abe
Karim <k.mehour@gmail.com>
2022-04-30 22:23:51 -0400

    finish exercises 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let stringToArray = string.split("")
+let reversedString = stringToArray.reverse().join("")
+return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    let args = Array.from(arguments).slice(1)
+    for (let arg of args){
+        removeIndex = array.indexOf(arg);
+        if (array.includes(arg)) {
+            array.splice(removeIndex, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
