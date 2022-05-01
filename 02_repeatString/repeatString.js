06a9a5af3563fa0d516900ec8c6b1564e4b51b32
Karim <k.mehour@gmail.com>
2022-04-30 21:39:04 -0400

    Finished exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, nTimes) {
+// Loop n times and add
+let longString = ""
+if(nTimes<0) {
+    return "ERROR"
+}
+for(let i = 0; i<nTimes; i++){
+    longString += string
+}
+return longString
 };
 
 // Do not edit below this line
