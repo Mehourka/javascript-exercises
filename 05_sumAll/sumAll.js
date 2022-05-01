55a109dec03217120287b0a8ad2cb68cd39ebe0e
Karim <k.mehour@gmail.com>
2022-04-30 23:04:55 -0400

    finish exercises 5 throught 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(a<0 || b<0 ||  !(typeof a == "number") || !(typeof b == "number")) return "ERROR";
 
+    let max = Math.max(a,b);    
+    let min = Math.min(a,b);
+    let sum = 0;
+    // start from upper number, and decrement in loop, adding that number till lower number
+    for(let numberBetween = max; numberBetween>=min; numberBetween--){
+        sum += numberBetween;
+
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(degree_f) {
+const degree_c = (degree_f-32)*5/9;
 
+return Math.round(degree_c*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(degree_c) {
+  const degree_f = degree_c*9/5+32;
+  return Math.round(degree_f*10)/10;
 };
 
 // Do not edit below this line
