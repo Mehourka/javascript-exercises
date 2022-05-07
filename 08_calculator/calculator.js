f6abc479440db7d4ab614c69ae7f59416d153b55
Karim <k.mehour@gmail.com>
2022-05-07 11:39:04 -0400

    Complete exercises 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a,b)=> a+b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  // multiply numbers in a array
+  return arr.reduce((a,b)=> a*b, 1)
 };
 
-const power = function() {
-	
+const power = function(x,p) {
+	return x**p;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  	let intArray = [...Array(n).keys()] // Array from 0 to n-1
+    intArray = intArray.map(e => e+1) // Array from 1 to n
+  return intArray.reduce((a,b)=> a*b,1)
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+function isLetter(l){
+    code = l.charCodeAt();
+    return code>= 65 && code <= 122;
+}
 
+const palindromes = function (string) {
+    let array = string
+                    .toLowerCase() 
+                    .split("") // turn string to array
+                    .filter(isLetter); // get only letters
+
+    for (let i=0; i< array.length/2; i++){
+        if(array[i] !== array[array.length -i-1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
