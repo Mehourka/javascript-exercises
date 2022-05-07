f723f6189cf2919c4ecb9af78243a61fa02aa627
Karim <k.mehour@gmail.com>
2022-05-07 15:48:37 -0400

    Complete Exercises 10, 11 and 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(nMax) {
+    let last =1;
+    let next = 1;
+    let holder = 0;
+    if (nMax<0) {
+        return "OOPS";
+    }
+    for(let i=0; i<nMax-2;i++){ // nMax-2 is an adjustment to skip the first two iterations. 
+        holder = last+next;
+        last = next;
+        next = holder;
+    }
+    return next;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+// const findTheOldest = function(array) {
+    
+//     let sorted = array.sort((a,b)=>{
+//         return getAge(b) - getAge(a); // Sort according to age (oldest to youngest)
+//     })   
+//     return sorted[0]; // Return first sorted element (oldest)
+// };
 
+
+const findTheOldest = function(array) {
+    let oldest = array.reduce((oldest, person)=>{
+        return (getAge(oldest) > getAge(person) ? oldest : person)
+    })
+    return oldest;
 };
+
+function getAge(personObj){
+    const currentYear = new Date().getFullYear();
+    const birthYear = personObj["yearOfBirth"];
+    const oldestYear = (personObj["yearOfDeath"] || currentYear)   ;
+    return oldestYear - birthYear;    
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
